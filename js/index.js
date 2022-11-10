600195cbb90fc2372ab6091cb960f5eeef5cc1f2
kcutter1 <106287154+kcutter1@users.noreply.github.com>
2022-11-09 20:54:00 -0500

    hw9

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("you're amazing");
+
+$('.v1_32').on("click", function(){
+
+    $('.v1_32').css('background-color', 'white');
+
+  });
+
+  $('.v1_33').on("click", function(){
+
+    $('.v1_33').css('background-color', 'white');
+
+  });
